e2bd9b444121d8177d26d46f3e34b342c43bee6a
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-11-06 09:29:48 -0300

    FEAT: header component started

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <a href="#">
+            <img src="../../public/img/Vector.png" alt="">
+        </a>
+    </header>
+</template>
+
+<style scoped>
+</style>
