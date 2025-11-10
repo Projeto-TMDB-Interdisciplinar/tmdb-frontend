365a1ab4d8f4cf5c471e1a6990da4f4b2fcbb155
giovana-rosa <giovana.rosaifc@gmail.com>
2025-11-10 10:56:27 -0300

    FEAT: Etapa "Alterando o store de gêneros" concluída.

--- src/stores/genre.js
+++ src/stores/genre.js
@@ -1,20 +1,25 @@
-import { reactive, computed } from 'vue';
-import { defineStore } from 'pinia';
-import api from '@/plugins/axios';
+import { reactive, computed } from 'vue'
+import { defineStore } from 'pinia'
+import api from '@/plugins/axios'
 
 export const useGenreStore = defineStore('genre', () => {
   const state = reactive({
     genres: [],
-  });
+    currentGenreId: null,
+  })
 
-  const genres = computed(() => state.genres);
-  const getGenreName = (id) =>
-    state.genres.find((genre) => genre.id === id).name;
+  const genres = computed(() => state.genres)
+  const getGenreName = (id) => state.genres.find((genre) => genre.id === id)?.name
 
   const getAllGenres = async (type) => {
-    const response = await api.get(`genre/${type}/list?language=pt-BR`);
-    state.genres = response.data.genres;
-  };
+    const response = await api.get(`genre/${type}/list?language=pt-BR`)
+    state.genres = response.data.genres
+  }
 
-  return { genres, getAllGenres, getGenreName };
-});
+  const currentGenreId = computed(() => state.currentGenreId)
+  const setCurrentGenreId = (genreId) => {
+    state.currentGenreId = genreId
+  }
+
+  return { genres, getAllGenres, getGenreName, currentGenreId, setCurrentGenreId }
+})
