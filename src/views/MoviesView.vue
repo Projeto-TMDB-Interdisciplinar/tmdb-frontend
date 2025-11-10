db2767ecf03d6be41417c245821d24bbb06449d9
giovana-rosa <giovana.rosaifc@gmail.com>
2025-11-10 11:35:43 -0300

    FEAT: Etapa "Criando o componente MovieDetailsView.vue" concluída.

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -3,6 +3,8 @@ import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
 import Loading from 'vue-loading-overlay'
 import { useGenreStore } from '@/stores/genre'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
 const genreStore = useGenreStore()
 
@@ -30,26 +32,45 @@ const listMovies = async (genreId) => {
 }
 
 const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
+
+function openMovie(movieId) {
+  router.push({ name: 'MovieDetails', params: { movieId } })
+}
 </script>
 
 <template>
   <h1>Filmes</h1>
   <ul class="genre-list">
-      <li v-for="genre in genreStore.genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item" :class="{ active: genre.id === genreStore.currentGenreId }">
-    {{ genre.name }}
-  </li>
+    <li
+      v-for="genre in genreStore.genres"
+      :key="genre.id"
+      @click="listMovies(genre.id)"
+      class="genre-item"
+      :class="{ active: genre.id === genreStore.currentGenreId }"
+    >
+      {{ genre.name }}
+    </li>
   </ul>
   <loading v-model:active="isLoading" is-full-page />
   <div class="movie-list">
     <div v-for="movie in movies" :key="movie.id" class="movie-card">
-      <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+      <img
+        :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+        :alt="movie.title"
+        @click="openMovie(movie.id)"
+      />
       <div class="movie-details">
         <p class="movie-title">{{ movie.title }}</p>
         <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
         <p class="movie-genres">
-          <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)" :class="{ active: genre_id === genreStore.currentGenreId }">
-  {{ genreStore.getGenreName(genre_id) }}
-</span>
+          <span
+            v-for="genre_id in movie.genre_ids"
+            :key="genre_id"
+            @click="listMovies(genre_id)"
+            :class="{ active: genre_id === genreStore.currentGenreId }"
+          >
+            {{ genreStore.getGenreName(genre_id) }}
+          </span>
         </p>
       </div>
     </div>
