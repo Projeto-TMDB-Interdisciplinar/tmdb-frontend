1d56cb9e68c21a1106aa7a8d9e93e40bc249eb49
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-11-17 11:58:56 -0300

    FEAT: Footer Component's structure and design

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="footer">
+    <div class="sobre">
+        <p>Sobre</p>
+        <ul>
+            <li>
+                <a href="">Equipe</a>
+            </li>
+            <li>
+                <a href="">Nossa Missão</a>
+            </li>
+        </ul>
+    </div>
+    <div class="contato">
+        <p>Contato</p>
+        <ul>
+            <li>
+                <a href="">cinelatin@gmail.com</a>
+            </li>
+            <li>
+                <a href="">(47) 99900-9900</a>
+            </li>
+            <li>
+                <a href="">Araquari - SC,Brazil</a>
+            </li>
+        </ul>
+    </div>
+    <div class="social">
+        <p>Redes Sociais</p>
+        <ul>
+            <li>
+                <a href="">
+                    <img src="../../public/img/instagram.png" alt="">
+                </a>
+            </li>
+            <li>
+                <a href=""><img src="../../public/img/youtube.png" alt=""></a>
+            </li>
+            <li>
+                <a href=""><img src="../../public/img/x.png" alt=""></a>
+            </li>
+        </ul>
+    </div>
+<hr>
+        <div class="copyright">
+            <p>CineLatin — explorando o cinema latino-americano.</p>
+            <p> © 2025 CineLatin. Dados fornecidos por The Movie Database (TMDB).</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .footer {
+        background-color: #3DF2FF;
+        padding: 2vw;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    ul li a {
+        text-decoration: none;
+        color:black ;
+    }
+    .social ul {
+        list-style: none;
+    }
+    hr {
+        background-color: black;
+        height: 2px;
+        border: none;
+    }
+    .copyright p {
+        text-align: center;
+    }
+</style>
